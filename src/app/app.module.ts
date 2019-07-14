eb3b2a6bad0bad3ca2983e75537a5b9fb93f271a
rutujafaldu94 <rfaldu@asu.edu>
2019-07-14 12:14:53 -0700

    drag and drop working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainComponent } from "./main/main.component";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainComponent],
+  imports: [BrowserModule, AppRoutingModule, DragDropModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from "@angular/core";
+import {
+  CdkDragDrop,
+  CdkDragEnd,
+  moveItemInArray,
+  transferArrayItem,
+  copyArrayItem
+} from "@angular/cdk/drag-drop";
+import { resetCompiledComponents } from "@angular/core/src/render3/jit/module";
+
+interface FunctionConfig {
+  name: string;
+  args: number;
+}
+
+@Component({
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"]
+})
+export class MainComponent implements OnInit {
+  function_names: FunctionConfig[] = [
+    { name: "add", args: 2 },
+    { name: "subtract", args: 2 },
+    { name: "foo", args: 3 },
+    { name: "doSomethingComplex", args: 1 }
+  ];
+
+  mainDivArray: FunctionConfig[] = [];
+  numberOfArgs: number[];
+
+  constructor() {}
+  ngOnInit() {}
+
+  droppedIntoMain(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer !== event.container) {
+      if (this.mainDivArray.length === 0) {
+        copyArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex
+        );
+        this.numberOfArgs = Array(this.mainDivArray[0].args).fill(0);
+      }
+    }
+  }
+
+  droppedIntoFunctionList(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer !== event.container) {
+      this.mainDivArray = [];
+      this.numberOfArgs = [];
+    }
+  }
+}
